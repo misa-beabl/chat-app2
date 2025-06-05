5fb3cebe57cda136b48fa8abc41f17b03da6ec1a
misa-beabl <misaki.inoue@smart-group.co.jp>
2025-06-05 15:16:29 +0900

    first commit

---
+++ src/test/java/in/tech_camp/chat_app2/ChatApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.chat_app2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
